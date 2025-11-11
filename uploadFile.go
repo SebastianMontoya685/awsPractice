7584bc14abb71474da47dd3b2199a4cab69a4ad0
SebastianMontoya685 <sebastian.m@jpi.global>
2025-11-12 01:55:29 +1100

    finished writing up the code for the aws cli executable that will allow me to programmatically upload files to my aws and notify a lambda function. the idea here is that we can perform future integrations and actions on a file upload, like telling a specific team about teh uplaoded file, once its uploaded.

--- uploadFile.go
+++ uploadFile.go
@@ -1,8 +1,75 @@
 package awsPractice
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+func uploadFile(filepath string, bucket string, key string) error {
+	// Opening the file
+	file, err := os.Open(filepath)
+	if err != nil {
+		return fmt.Errorf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	// Creating a new AWS session
+	awsSession, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return fmt.Errorf("Error creating AWS session: %v", err)
+	}
+
+	// Creating a new S3 client
+	s3Client := s3.NewFromConfig(awsSession)
+
+	// Uploading the file to the s3 bucket
+	_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   file,
+	})
+	if err != nil {
+		return fmt.Errorf("Error uploading file: %v", err)
+	}
+
+	return nil
+}
+
+func notifyLambda(payload map[string]string) error {
+	// Creating a new AWS session
+	awsSession, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return fmt.Errorf("Error creating AWS session: %v", err)
+	}
+
+	// Creating a new lambda client
+	lambdaClient := lambda.NewFromConfig(awsSession)
+
+	// Marshalling the payload into bytes
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("Error marshalling payload: %v", err)
+	}
+
+	// Invoking the lambda function
+	_, err = lambdaClient.Invoke(context.TODO(), &lambda.InvokeInput{
+		FunctionName: aws.String(payload["lambdaName"]),
+		Payload:      payloadBytes,
+	})
+	if err != nil {
+		return fmt.Errorf("Error invoking lambda function: %v", err)
+	}
+
+	return nil
+}
 
 func main() {
 	// Parsing the bucket, key, lambda, and filepath flags from the command line
@@ -12,4 +79,37 @@ func main() {
 	lambdaName := flag.String("lambda", "", "Name of the Lambda function that we want to notify")
 	flag.Parse()
 
+	// Uploading the file to the s3 bucker using a goroutine
+	s3Upload := make(chan error)
+
+	go func() {
+		s3Upload <- uploadFile(*filepath, *bucket, *key)
+	}()
+
+	err := <-s3Upload
+	if err != nil {
+		fmt.Println("Error uploading file: %v", err)
+		return
+	}
+
+	// Notifying a lambda function using a goroutine, and also using AWS SQS to send a message to receivers
+	lambdaUpload := make(chan error)
+
+	// Configuring the payload for the lambda function
+	payload := map[string]string{
+		"bucket":     *bucket,
+		"key":        *key,
+		"lambdaName": *lambdaName,
+	}
+	go func() {
+		lambdaUpload <- notifyLambda(payload)
+	}()
+
+	err = <-lambdaUpload
+	if err != nil {
+		fmt.Printf("Error notifying lambda function: %v", err)
+		return
+	}
+
+	fmt.Println("File uploaded and lambda function notified successfully")
 }
